a2c9ada83c44e9c93578cfaffdc9c8e1bb9fa23d
吴小白 <296015668@qq.com>
2022-08-18 18:54:33 +0800

    feat: 更新 v2.25.0

---
+++ web/docker-entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+
+if [ -f "/etc/nginx/sites-enabled/jumpserver.conf" ]; then
+  cp -rf /etc/nginx/sites-enabled/jumpserver.conf /etc/nginx/conf.d/default.conf
+fi
+
+if [ "${USE_IPV6}" == "1" ]; then
+  sed -i 's@# listen \[::\]:80@listen \[::\]:80@g' /etc/nginx/conf.d/default.conf
+  sed -i 's@# listen \[::\]:443@listen \[::\]:443@g' /etc/nginx/conf.d/default.conf
+fi
+
+if [ -n "${SERVER_NAME}" ]; then
+  sed -i "s@# server_name .*;@server_name ${SERVER_NAME};@g" /etc/nginx/conf.d/default.conf
+fi
+
+if [ -n "${SSL_CERTIFICATE}" ] && [ -f "/etc/nginx/cert/${SSL_CERTIFICATE}" ]; then
+  sed -i "s@ssl_certificate .*;@ssl_certificate cert/${SSL_CERTIFICATE};@g" /etc/nginx/conf.d/default.conf
+fi
+
+if [ -n "${SSL_CERTIFICATE_KEY}" ] && [ -f "/etc/nginx/cert/${SSL_CERTIFICATE_KEY}" ]; then
+  sed -i "s@ssl_certificate_key .*;@ssl_certificate_key cert/${SSL_CERTIFICATE_KEY};@g" /etc/nginx/conf.d/default.conf
+fi
+
+if [ -n "${CLIENT_MAX_BODY_SIZE}" ]; then
+  sed -i "s@client_max_body_size .*;@client_max_body_size ${CLIENT_MAX_BODY_SIZE};@g" /etc/nginx/conf.d/default.conf
+fi
+
+if [ -f "/etc/init.d/cron" ]; then
+  /etc/init.d/cron start
+fi
+
+nginx -g "daemon off;"
